83eac0b2038cfce2f04970f738dedf2a3c39b7d2
Roz <san7yx@gmail.com>
2023-04-30 19:08:02 +0000

    Delete html

--- pages/index.tsx
+++ pages/index.tsx
@@ -95,7 +95,7 @@ const Home: NextPage = () => {
         const firstDelimiterIndex = buffer.indexOf("```");
         if (firstDelimiterIndex >= 0) {
           // Remove the data before the first delimiter
-          buffer = buffer.slice(firstDelimiterIndex + 3);
+          buffer = buffer.slice(firstDelimiterIndex + 7);
           foundFirstDelimiter = true;
         }
       }
